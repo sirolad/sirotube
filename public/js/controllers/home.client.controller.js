936f3bad17742339af4a9dc7fb36f4554c3265e4
andela-sakande <suraj.akande@getitllc.com>
2017-04-12 15:50:14 +0100

    Remove placeholder on landing page

---
+++ public/js/controllers/home.client.controller.js
@@ -0,0 +1,10 @@
+app.controller('HomeController', ['$scope','$http','toastr','User', 'Video', function($scope, $http, toastr, User, Video) {
+
+  $scope.listVideos =  function(){
+    Video.retrieveAll().then(function(response){
+       $scope.allVideos = response.data;
+    });
+  };
+
+  $scope.listVideos();
+}]);
